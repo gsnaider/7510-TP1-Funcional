cb3e7a617dab885b73abf4c71b2ffbf2077dd3c7
Gaston Snaider <gastonsnaider93@gmail.com>
2017-09-03 18:38:39 -0300

    Create input-parser and input-parser-test
    This already has part of the functionality and tests for translating a string database to Facts.

---
+++ src/input_parser.clj
@@ -0,0 +1,47 @@
+(ns input-parser
+  (:require [data-model]
+            [clojure.string :as str])
+  (:import [data_model Fact]))
+
+(def rule-assign-code ":-")
+
+(defn- get-lines
+  "Returns a list containing each trimmed line from database, excluding blank lines."
+  [database]
+  (->> database
+      (str/split-lines)
+      (map #(str/trim %))
+      (remove str/blank?)))
+
+(defn- valid-fact?
+  "Returns true if the string fact-line is a valid fact, or false otherwise."
+  [fact-line]
+  ;; TODO: implement.
+  true)
+
+(defn- get-name
+  "Returns the Fact name from the fact-line."
+  [fact-line]
+  ;; TODO: implement.
+  nil)
+
+(defn- get-params
+  "Returns the Fact parameters from the fact-line as a list of strings."
+  [fact-line]
+  ;; TODO: implement.
+  nil)
+
+(defn- line->fact
+  "Converts an input string line to a Fact, or throws an Exception if the conversion is not possible."
+  [fact-line]
+  (if (valid-fact? fact-line)
+    (new Fact (get-name fact-line) (get-params fact-line))
+    (throw (IllegalArgumentException. "Invalid fact."))))
+
+(defn get-facts
+  "Returns a list containing all the facts from the database,
+  or throws an Exception if the databse can't be parsed."
+  [database]
+  (->> (get-lines database)
+        (remove #(str/includes? % rule-assign-code))
+        (map #(line->fact %))))
