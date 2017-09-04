2382eceb56346b7a8a70aba136abdf1810f1bc7b
Gaston Snaider <gastonsnaider93@gmail.com>
2017-09-04 01:11:47 -0300

    Complete fact evaluation logic

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -7,6 +7,11 @@
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
   [database query]
-  (contains?
-    (fact-parser/get-facts database)
-    (data-model/query->fact (query-parser/get-query query))))
+  (try
+    (contains?
+      (fact-parser/get-facts database)
+      (data-model/query->fact (query-parser/get-query query)))
+  (catch Exception e
+    (do
+      (println (str "Error while parsing input data: " (.getMessage e)))
+      nil))))
