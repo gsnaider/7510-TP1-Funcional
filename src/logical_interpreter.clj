815e86dc85b6cb942c2d82e431cfb507968ffd1b
Gaston Snaider <gastonsnaider93@gmail.com>
2017-09-06 21:13:59 -0300

    Add skeleton for rule evaluation logic

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,16 +1,34 @@
 (ns logical-interpreter
   (:require [data-model]
             [fact-parser]
+            [rule-parser]
             [query-parser]))
+
+(defn- rule-in-database?
+  "Returns true if the Rule represented by query is in rules, and its corresponding
+  facts are in the database facts, or false otherwise."
+  [facts rules query]
+  false
+  )
+
+(defn- fact-in-database?
+  "Returns true if the Fact represented by query is in facts, or false otherwise."
+  [facts query]
+  (contains? facts (data-model/query->fact query))
+  )
 
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
-  [database query]
+  [database query-line]
   (try
-    (contains?
-      (fact-parser/get-facts database)
-      (data-model/query->fact (query-parser/get-query query)))
+    (let [facts (fact-parser/get-facts database)
+          rules (rule-parser/get-rules database)
+          query (query-parser/get-query query-line)]
+    (if (fact-in-database? facts query)
+        true
+        (rule-in-database? facts rules query)
+    ))
   (catch Exception e
     (do
       (println (str "Error while parsing input data: " (.getMessage e)))
