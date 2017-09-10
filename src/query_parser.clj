353075223b9bdbd45b3af0d0535547540a315a3e
Gaston Snaider <gastonsnaider93@gmail.com>
2017-09-10 13:49:13 -0300

    Move query-validation logic to query-validator.

--- src/query_parser.clj
+++ src/query_parser.clj
@@ -1,20 +1,14 @@
 (ns query-parser
   (:require [data-model]
             [parser-util]
+            [query-validator]
             [clojure.string :as str])
   (:import [data_model Query]))
-
-(def query-regex #"^\w+\((\w+)(, \w+)*\)$")
-
-(defn- valid-query?
-  "Returns true if the query string is a valid query, or false otherwise."
-  [query]
-  (not (nil? (re-matches query-regex query))))
 
 (defn get-query
   "Returns a Query obtained from parsing a query stirng,
   or throws an Exception if the parsing is not possible."
   [query]
-  (if (valid-query? query)
+  (if (query-validator/valid-query? query)
     (new Query (parser-util/get-name query) (parser-util/get-params query))
     (throw (IllegalArgumentException. "Invalid query."))))
