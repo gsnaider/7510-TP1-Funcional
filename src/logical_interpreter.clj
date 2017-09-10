2f56753f8d9500f51dc07ad763d7a8ce0aeac065
Gaston Snaider <gastonsnaider93@gmail.com>
2017-09-10 20:33:50 -0300

    Refactor of logical interpreter.
    Rename methods.
    Fix doc.
    Move rule comparison with query logic to data-model.

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -8,14 +8,11 @@
 (defn- find-rule
   "Returns the rule in rules with the same name as query, or nil if there's no such rule."
   [rules query]
-    (first (filter (fn[rule](= (:name rule) (:name query))) rules))
-  )
-
-(defn- params-match?
-  "Returns true if the number of params from rule match the number of params from query, or false otherwise."
-  [rule query]
-    (= (count (:params rule)) (count (:params query)))
-  )
+    (->> rules
+      (filter 
+        (fn[rule]
+        (data-model/rule-equals-query? rule query)))
+      first))
 
 (defn- evaluate-params
   "Returns a set of facts that come from evaluating each X, Y,..
@@ -29,36 +26,31 @@
             (map #(get params-map %) (:params fact))))
       (:facts rule))))
 
-(defn- fact-in-db?
-  "Returns true if the Fact represented by Query is in facts, or false otherwise."
+(defn- fact-in-database?
+  "Returns true if the Fact represented by query is in facts, or false otherwise."
   [facts query]
   (contains?
     facts
     (data-model/query->fact query)))
 
-(defn- facts-in-db?
-  "Returns true if all the facts from rule, evaluated with the params from query, are in facts."
+(defn- facts-in-database?
+  "Returns true if all the facts from rule, evaluated using the parameters from query, are in facts."
   [facts rule query]
     (let [params (zipmap (:params rule) (:params query))
           concrete-facts (evaluate-params rule params)]
     (reduce (fn[x y] (and x y)) 
-      (map #(fact-in-db? facts %) concrete-facts)
+      (map #(fact-in-database? facts %) concrete-facts)
       )
     )
   )
 
-(defn- rule-in-db?
+(defn- rule-in-database?
   "Returns true if the Rule represented by query is in rules,
   and each fact associated to that rule is in facts."
 	[facts rules query]
   (if-let [rule (find-rule rules query)]
-    (if (params-match? rule query)
-      (facts-in-db? facts rule query)
-      false
-    )
-    false 
-  )
-)
+    (facts-in-database? facts rule query)
+    false))
 
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
@@ -69,10 +61,9 @@
           rules (rule-parser/parse-rules database)
           query (query-parser/parse-query query)]
 
-  	(if (fact-in-db? facts query)
+  	(if (fact-in-database? facts query)
   		true
-  		(rule-in-db? facts rules query)
-    ))
+  		(rule-in-database? facts rules query)))
   (catch Exception e
     (do
       (println (str "Error while parsing input data: " (.getMessage e)))
